4b6f8897d11c6bb7e49fdebab801665219b6990d
Alessandro Pino <alessandro@profissionalti.com>
2022-01-20 09:58:42 -0300

    Revert "Feature/modificar estilos componente nova transferencia"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NovaTransferenciaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
