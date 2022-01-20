183090615f0ac3b7e82be165e03001d914e336e4
Alessandro Pino <alessandro@profissionalti.com>
2022-01-20 20:30:49 -0300

    configurado output property

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -8,13 +8,17 @@ import { Component } from "@angular/core";
 
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number = 222;
 
   transferir() {
     console.log('Solicitada nova transferencia');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    //console.log('Valor: ', this.valor);
+    //console.log('Destino: ', this.destino);
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+
   }
 }
